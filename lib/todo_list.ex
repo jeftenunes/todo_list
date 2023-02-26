30eca78407a54fb95e5465773c73fc6248aa1b13
jeftenunes <jeftenunes@hotmail.com>
2023-02-26 18:45:49 -0500

    add entry - processes

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,3 +1,50 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:entries, entries} -> entries
+    after
+      5000 -> {:err, :timeout}
+    end
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    IO.puts("Processing entries message")
+    send(caller, {:entries, TodoList.entries(todo_list, date)})
+
+    todo_list
+  end
+
+  defp process_message(todo_list, {:add, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry.title, new_entry.description, new_entry.date)
+  end
+
+  defp process_message(todo_list, {:update, id}) do
+  end
+
+  defp process_message(todo_list, {:delete, id}) do
+    TodoList.delete_at(todo_list, id)
+  end
+end
+
 defmodule TodoList do
   defstruct auto_id: 0, entries: %{}
 
